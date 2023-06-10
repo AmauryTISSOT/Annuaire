5543dc84251499f429a0d72e294f2f272425a447
amaury <tissot.amaury@gmail.com>
2023-06-10 22:10:00 +0200

    Update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { useState } from "react";
+import DataDisplay from "./components/DataDisplay";
+import SearchBar from "./components/SearchBar";
+import { icon } from "./constants/constants";
+
 function App() {
+  const [parentState, setParentState] = useState("");
+
   return (
-    <>
-      <h1 className="text-center">TEST</h1>
-    </>
+    <div className="w-screen h-screen flex items-center justify-center flex-col">
+      {!parentState && (
+        <>
+          <span>{icon.book}</span>
+          <h1 className="text-center m-5 font-bold text-2xl">
+            ANNUAIRE DES ETABLISSEMENTS PUBLICS
+          </h1>
+          <SearchBar setState={setParentState} />
+        </>
+      )}
+      {parentState && <DataDisplay parentState={parentState} />}
+    </div>
   );
 }
 
