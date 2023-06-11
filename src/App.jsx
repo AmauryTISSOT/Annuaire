9fce55a8268efc6db71f756544ad07c8e2f2a18b
amaury <tissot.amaury@gmail.com>
2023-06-11 19:30:24 +0200

    Add reactRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,12 @@
-import { useState } from "react";
-import DataDisplay from "./components/DataDisplay";
-import SearchBar from "./components/SearchBar";
-import { icon } from "./constants/constants";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  const [parentState, setParentState] = useState("");
-
   return (
-    <div className="w-screen h-screen flex items-center justify-center flex-col">
-      {!parentState && (
-        <>
-          <span>{icon.book}</span>
-          <h1 className="text-center m-5 font-bold text-2xl">
-            ANNUAIRE DES ETABLISSEMENTS PUBLICS
-          </h1>
-          <SearchBar setState={setParentState} />
-        </>
-      )}
-      {parentState && <DataDisplay parentState={parentState} />}
+    <div>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
