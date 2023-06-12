fdc00fec6ef1c958ed0eb502af5d4f8f72fa8bd2
amaury <tissot.amaury@gmail.com>
2023-06-12 20:30:57 +0200

    Pass parent state add Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { Route, Routes } from "react-router-dom";
+import { useState } from "react";
 import Home from "./pages/Home";
+import Atlas from "./pages/Atlas";
 
 function App() {
+  const [parentState, setParentState] = useState("");
+
   return (
     <div>
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route path="/" element={<Home setParentState={setParentState} />} />
+        <Route path="/atlas" element={<Atlas parentState={parentState} />} />
       </Routes>
     </div>
   );
