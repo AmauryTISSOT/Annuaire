dfcf885e99142edfe5e275947903b5c5342c0c09
amaury <tissot.amaury@gmail.com>
2023-06-12 20:32:19 +0200

    Rework page add github link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,30 @@
-import DataDisplay from "../components/DataDisplay";
+/* eslint-disable react/prop-types */
 import SearchBar from "../components/SearchBar";
 import { icon } from "../constants/constants";
-import { useState } from "react";
 
-const Home = () => {
-  const [parentState, setParentState] = useState("");
-  //TODO: navigation
-
+const Home = ({ setParentState }) => {
   return (
     <>
-      {!parentState && (
-        <div className="w-screen h-screen flex items-center justify-center flex-col">
-          <>
-            <span>{icon.book}</span>
-            <h1 className="text-center m-5 font-bold text-2xl">
-              ANNUAIRE DES ETABLISSEMENTS PUBLICS
-            </h1>
-            <SearchBar setState={setParentState} />
-          </>
+      <div className="w-screen h-screen flex items-center justify-center flex-col">
+        <span>{icon.book}</span>
+        <h1 className="text-center m-5 font-bold text-2xl">
+          ANNUAIRE DES ETABLISSEMENTS PUBLICS
+        </h1>
+        <SearchBar setParentState={setParentState} />
+        <div className="flex flex-col justify-center items-center fixed bottom-0 left-0 w-full">
+          <p>Réalisé par Amaury Tissot</p>
+          <p>Application à usage strictement personnel</p>
+          <a
+            href="https://github.com/AmauryTISSOT/Annuaire"
+            target={"_blank"}
+            rel="noreferrer"
+            className="my-4 hover:animate-spin"
+            data-testid="github-link"
+          >
+            <span>{icon.github}</span>
+          </a>
         </div>
-      )}
-      {parentState && <DataDisplay parentState={parentState} />}
+      </div>
     </>
   );
 };
